495dcc4299ee981522fd5afb576b9758c6d1fca5
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-10-12 21:59:05 +0300

    Lesson #3

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 
 import Lesson1 from './src/lesson_1';
 import Lesson2 from './src/lesson_2';
+import Lesson3 from './src/lesson_3';
 
 export default function App() {
   return (
-    <Lesson2 />
+    <Lesson3 />
   );
 }
