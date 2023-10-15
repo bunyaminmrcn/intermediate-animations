bbead37a4e0ed87812091a0920265db39bd7603a
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-10-15 12:17:40 +0300

    Lesson #9 --requireFix

--- App.tsx
+++ App.tsx
@@ -9,9 +9,10 @@ import Lesson5 from './src/lesson_5';
 import Lesson6 from './src/lesson_6';
 import Lesson7 from './src/lesson_7';
 import Lesson8 from './src/lesson_8';
+import Lesson9 from './src/lesson_9'
 
 export default function App() {
   return (
-    <Lesson8 />
+    <Lesson9 />
   );
 }
